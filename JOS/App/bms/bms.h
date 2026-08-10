0ec62f40a7bd9d3feb94443b24534e8ac13b552f
Nicola <nicola@local>
2026-08-10 16:45:55 +0200

    feat(bms): init SPI master to EPS STM32L496 (#37)

--- JOS/App/bms/bms.h
+++ JOS/App/bms/bms.h
@@ -1,10 +1,17 @@
 #ifndef BMS_H
 #define BMS_H
 
+#include <stdbool.h>
+
 #include "obsw_types.h"
 
-/* Initialise BMS interface */
+/* Initialise BMS interface (brings up the subsystem SPI master to the EPS) */
 void bms_init(void);
+
+/* True once the subsystem SPI master to the EPS STM32L496 is initialised and
+   ready. False means bms_get_status() is serving boot defaults, not EPS
+   telemetry. */
+bool bms_spi_ready(void);
 
 /* Get current battery status */
 bms_status_t bms_get_status(void);
