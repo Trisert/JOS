66fdfd1c9434037ddc7506c956c8caad4105f22f
root <root@vmi3545376.contaboserver.net>
2026-09-09 13:43:41 +0200

    fix(comms): narrow sha256 t1/t2 scope into compression loop (cppcheck variableScope)

--- JOS/App/comms/sha256.c
+++ JOS/App/comms/sha256.c
@@ -30,7 +30,7 @@ static const uint32_t k[64] = {
 };
 
 void sha256_transform(SHA256_CTX *ctx, const uint8_t data[]) {
-	uint32_t a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
+	uint32_t a, b, c, d, e, f, g, h, i, j, m[64];
 	for (i = 0, j = 0; i < 16; ++i, j += 4)
 		m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
 	for ( ; i < 64; ++i)
@@ -38,8 +38,8 @@ void sha256_transform(SHA256_CTX *ctx, const uint8_t data[]) {
 	a = ctx->state[0]; b = ctx->state[1]; c = ctx->state[2]; d = ctx->state[3];
 	e = ctx->state[4]; f = ctx->state[5]; g = ctx->state[6]; h = ctx->state[7];
 	for (i = 0; i < 64; ++i) {
-		t1 = h + EP1(e) + CH(e, f, g) + k[i] + m[i];
-		t2 = EP0(a) + MAJ(a, b, c);
+		uint32_t t1 = h + EP1(e) + CH(e, f, g) + k[i] + m[i];
+		uint32_t t2 = EP0(a) + MAJ(a, b, c);
 		h = g; g = f; f = e; e = d + t1; d = c; c = b; b = a; a = t1 + t2;
 	}
 	ctx->state[0] += a; ctx->state[1] += b; ctx->state[2] += c; ctx->state[3] += d;
